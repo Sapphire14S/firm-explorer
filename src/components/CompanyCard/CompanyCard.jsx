026aaabf509d3d31f888e3d541431459b5ce2194
Vank414 <vankkumar14@gmail.com>
2026-06-22 16:40:45 +0530

    update company logo

--- src/components/CompanyCard/CompanyCard.jsx
+++ src/components/CompanyCard/CompanyCard.jsx
@@ -8,14 +8,15 @@ function CompanyCard({ name }) {
 
   const domain = name.toLowerCase();
 
-  const logoUrl = `https://img.logo.dev/${domain}.com?token=demo`;
+  // const logoUrl = `https://img.logo.dev/${domain}.com?token=demo`;
+  // const logoUrl = `https://logo.clearbit.com/${domain}.com`;
 
-  const website = `https://${domain}`;
+  const website = `https://${domain}.com`;
   const linkedin = `https://www.linkedin.com/company/${domain}`;
   const github = `https://github.com/${domain}`;
   const youtube = `https://www.youtube.com/@${name}`;
   const instagram = `https://www.instagram.com/${name}`;
-  const careers = `https://${domain}/careers`;
+  const careers = `https://${domain}.com/careers`;
 
 
   useEffect(() => {
@@ -43,7 +44,30 @@ function CompanyCard({ name }) {
   return (
     <div className="company-card">
 
-      <img src={logoUrl} alt={name} className="company-logo" />
+      {/* <img src={logoUrl} alt={name} className="company-logo" /> */}
+      {/* <img
+        src={logoUrl}
+        alt={name}
+        className="company-logo"
+        onError={(e) => {
+          e.target.src =
+            "https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg";
+        }}
+      /> */}
+
+      {info?.thumbnail?.source ? (
+        <img
+          src={info.thumbnail.source}
+          alt={name}
+          className="company-logo"
+        />
+      ) : (
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg"
+          alt="No Logo"
+          className="company-logo"
+        />
+      )}
 
       <h2 className="company-name">{name}</h2>
 
